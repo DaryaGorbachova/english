05e11b3e062492d72cd421ceb56f85b8a1ab4768
DaryaGorbachova <vrindavaneshvari@mail.ru>
2022-07-12 21:27:13 +0300

    frog story begins. text entered basic styling done

---
+++ be-careful-what-you-say/script.js
@@ -0,0 +1,21 @@
+const readmodebtn = document.getElementById('read-mode');
+const text = document.querySelector('.back');
+
+
+  let mode = 'off'
+readmodebtn.addEventListener('click', () => {
+    if(mode == 'off') {
+        mode = 'on';
+    text.style.backgroundColor = 'var(--beige)';
+    } else if (mode == 'on') {
+        mode = 'off';
+    text.style.backgroundColor = 'white';
+
+    }
+})
+
+
+
+function selfunc(x) {
+    window.getSelection().selectAllChildren(x);
+}
